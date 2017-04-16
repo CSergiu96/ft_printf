e33e32911889b066df950dd0f50ee79b146378a3
CSergiu96 <cuciurean.sergiu@yahoo.com>
2017-04-16 23:04:05 +0300

    script to compile and test

---
+++ test.sh
@@ -0,0 +1,7 @@
+make re
+gcc -o ex -Wall -Werror -Wextra main.c ftprintf.a -Iincludes
+clear
+echo -e "\n/********************************************************\ \n *\t\t\t\t\t\t\t* \n * \t\t    File output\t\t\t\t*\n *\t\t\t\t\t\t\t* \n/********************************************************\ \n\n" 
+./ex
+echo -e "\n\n"
+rm ex
